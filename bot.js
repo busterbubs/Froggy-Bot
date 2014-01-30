d7c4304bfc06047529f0961d96f68116c00b7da5
Frogboy <nickbarstatis@yahoo.com>
2014-01-30 16:42:34 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+var PlugAPI = require('./plugapi'); // git clone (or unzip) into the same directory as your .js file. There should be plugapi/package.json, for example (and other files)
+var ROOM = 'chillout-mixer-ambient-triphop';
+//Plug changed the update code again, as well as how it is stored, which broke my static method which could be used to get it. So, for now, it must be hardcoded.
+
+Current update code: p9R*
+
+// Instead of providing the AUTH, you can use this static method to get the AUTH cookie via twitter login credentials:
+PlugAPI.getAuth({
+    username: 'xxx',
+    password: 'xxx'
+}, function(err, auth) { // if err is defined, an error occurred, most likely incorrect login
+    if(err) {
+        console.log("An error occurred: " + err);
+        return;
+    }
+    var bot = new PlugAPI(auth, 'p9R*');
+    bot.connect(ROOM);
